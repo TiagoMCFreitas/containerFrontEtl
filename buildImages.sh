a7beaefef2a73008f748df9687bde107cde17d2c
TiagoMCFreitas <tiagofreitas85862@gmail.com>
2025-04-03 20:03:02 -0300

    rodar sistema front

---
+++ buildImages.sh
@@ -0,0 +1,12 @@
+version='v1' # Versao Inicial
+#echo "TROCAR A VERSAO DAS IMAGENS EM docker-compose.yml"
+#sed -i'' -e 's/:v1/:v2/g' docker-compose.yml
+
+echo "GERAR IMAGENS FRONT_ETL ${version}"
+
+echo "FRONT ETL"
+git clone git@github.com:tiagoKurt/Front-ETL.git servicos/frontend
+cd servicos/frontend
+bash buildImages.sh $version
+rm -rf frontend
+cd ../..
